2e87203b6b917c8b50fdbdac83b1226e1d4b99e3
Vatsal Sevak <vsevak@zendesk.com>
2020-06-01 23:54:14 -0700

    update-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Your account information was successfully updated'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash["notice"] = "Welcome to the Alpha Blog #{@user.username}, you have successfully singed up !!"
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully singed up !!"
       redirect_to articles_path
     else
       render 'new'
